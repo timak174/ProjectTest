91571a4289053cf3b9a9c0b9870fee9ee92778a7
timak09.06 <timak_vaa@mail.ru>
2026-06-23 12:33:26 +0500

    Email_23.06

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -11,25 +12,31 @@ import static com.codeborne.selenide.Selenide.*;
 //    @Test
 //    void successfulSearchTest(){
 //        open("https://www.ya.ru/");
-//        $("#text").setValue("selenoide").pressEnter();
+//        $("#text").setValue("selenide").pressEnter();
 //        $("serp-item").shouldBe();
 //
 //    }
 //}
 
 public class WebTest {
+    // Анатация @BeforeEach - тесты стоновят без прикондишен open(), в тесте остаются только шаги.
+    @BeforeEach
+    void setUp(){
+        open("https://duckduckgo.com/");
+    }
+
     //Так выглядит баозый авто-тест-кейс
     @Test
     @Tag("BLOCKER")
     //Важность выражается через TAG
-    @DisplayName("Для поискового запроса должен 'selenoide' отправляться не пустой список")
+    @DisplayName("Для поискового запроса должен 'selenide' отправляться не пустой список")
     // Название кейса.
     void successfulSearchTest(){
         //Шаги
-        open("https://duckduckgo.com/");
+        //open("https://duckduckgo.com/");
         //1.Открыть браузер (open)
         //2.Открыть путь (Пример(https://duckduckgo.com/))
-        $("#searchbox_input").setValue("selenoide").pressEnter();
+        $("#searchbox_input").setValue("selenide").pressEnter();
         //3.Ввести значение $().setValue
         //4.Совершить действие .pressEnter()
         $$("[data-testid='result'] li[data-layout='organic']").shouldBe();
@@ -38,9 +45,9 @@ public class WebTest {
     }
     @Test
     @Tag("BLOCKER")
-    @DisplayName("Для поискового запроса должен 'selenoide' отправляться не пустой список фото")
+    @DisplayName("Для поискового запроса должен 'selenide' отправляться не пустой список фото")
     void successfulSearchPhotoTest(){
-        open("https://duckduckgo.com/");
+        //open("https://duckduckgo.com/");
         $("#searchbox_input").setValue("selenoide").pressEnter();
         $("[class='kFFXe30DOpq5j1hbWU1q SnptgjT2zdOhGYfNng6g wZ4JdaHxSAhGy1HoNVja d26Geqs1C__RaCO7MUs2']").click();
         $$("SZ76bwIlqO8BBoqOLqYV").shouldBe();
